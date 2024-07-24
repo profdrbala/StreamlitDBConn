5c07a5c2cee3194770756010aca17dc81112afda
Dr. Bala Dhandayuthapani V. <dhanssoft@gmail.com>
2024-07-25 00:24:51 +0400

    Update DBConn.py

--- DBConn.py
+++ DBConn.py
@@ -4,17 +4,18 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 
 # Initialize Firebase Admin SDK
-cred = credentials.Certificate('firebase-key.json')
-firebase_admin.initialize_app(cred)
+#cred = credentials.Certificate('firebase-key.json')
+app = firebase_admin.initialize_app(credentials.Certificate('firebase-key.json'), name='DB_Conn')
+#firebase_admin.initialize_app(cred)
 
 # Get a Firestore client
-db = firestore.client()
+#db = firestore.client()
+db = firestore.client(app=app)
 
 # Streamlit App
 def main():
     st.title("Streamlit Firebase Example")
-    
-    # Example: Display data from Firebase
+
     st.subheader("Data from Firebase")
     #collection_name = "Books"  # Replace with your Firestore collection name
 
@@ -34,6 +35,27 @@ def main():
     # Let's see what we got!
     st.write("The id is: ", doc.id)
     st.write("The contents are: ", doc.to_dict())
+
+    title = st.text_input("Enter Title:")
+    isbn = st.number_input("Enter ISBN:")
+    author = st.number_input("Enter author:")
+    publisher = st.number_input("Enter publisher:")
+    country = st.number_input("Enter countyr:")
+    price = st.number_input("Enter price:")
+    
+    if st.button("Add Data"):
+        # Add data to Firestore
+        data = {
+            'title': name,
+            'isbn': isbn,
+            'author':author,
+            'pubisher':publisher,
+            'country':country,
+            'price':price
+        }
+        db.collection("Books").add(data)
+        st.success("Data added successfully!")
+        
 
 
 if __name__ == '__main__':
